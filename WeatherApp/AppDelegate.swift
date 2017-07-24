7a2702fd7cad0b7d697c238ecfce0d75733462df
Pavel Kazantsev <pv.kazantsev@gmail.com>
2017-07-24 21:55:16 +0700

    Initial commit.

---
+++ WeatherApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Pavel Kazantsev on 7/24/17.
+//  Copyright © 2017 PaKaz.net. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Pavel Kazantsev on 7/24/17.
+//  Copyright © 2017 PaKaz.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
